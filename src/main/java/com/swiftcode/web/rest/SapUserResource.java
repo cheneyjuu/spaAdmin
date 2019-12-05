5a0540446bcf85110529046d334bf220b60f8f99
巨臣 <cheneyjuu@163.com>
2019-12-05 11:24:58 +0800

    chore(SapUserResource): add swagger api

--- src/main/java/com/swiftcode/web/rest/SapUserResource.java
+++ src/main/java/com/swiftcode/web/rest/SapUserResource.java
@@ -3,6 +3,8 @@ package com.swiftcode.web.rest;
 import com.swiftcode.service.SapUserService;
 import com.swiftcode.service.dto.SapUserDTO;
 import com.swiftcode.service.util.CommonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/api/sap")
+@Api(tags = "SapUserResource")
 public class SapUserResource {
     private SapUserService service;
 
@@ -20,6 +23,7 @@ public class SapUserResource {
         this.service = service;
     }
 
+    @ApiOperation("SAP USER 注册")
     @PostMapping("/users")
     public CommonResult<SapUserDTO> addSapUser(@RequestBody SapUserDTO dto) {
         SapUserDTO sapUser = service.addSapUser(dto);
