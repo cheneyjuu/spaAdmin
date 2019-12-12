ae05a12cee4c7340892e6bccade282f2336b3fbe
RuiZhaohui <570798173@qq.com>
2019-12-12 21:44:40 +0800

    修改部分登录请求

--- src/main/java/com/swiftcode/service/SapUserService.java
+++ src/main/java/com/swiftcode/service/SapUserService.java
@@ -5,11 +5,14 @@ import com.swiftcode.domain.SapUser;
 import com.swiftcode.repository.SapUserRepository;
 import com.swiftcode.service.dto.SapUserDTO;
 import com.swiftcode.service.mapper.SapUserMapper;
+import com.swiftcode.web.rest.vm.ManagedSapUserVm;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -35,8 +38,38 @@ public class SapUserService {
         return mapper.toDto(sapUser);
     }
 
+    @Transactional(rollbackFor = Exception.class)
+    public SapUserDTO updateSapUser(ManagedSapUserVm sapUserVm) {
+        SapUser user = repository.findByUserCode(sapUserVm.getPERNR()).orElse(new SapUser());
+        SapUser aUser = mapper.toEntity(sapUserVm);
+        aUser.setId(user.getId());
+        if (!StringUtils.isEmpty(sapUserVm.getPassword())) {
+            aUser.setPassword(passwordEncoder.encode(sapUserVm.getPassword()));
+        } else {
+            aUser.setPassword(user.getPassword());
+        }
+        if (!StringUtils.isEmpty(sapUserVm.getPhoneNumber())) {
+            aUser.setPhoneNumber(sapUserVm.getPhoneNumber());
+        } else {
+            aUser.setPhoneNumber(user.getPhoneNumber());
+        }
+        SapUser sapUser = repository.save(aUser);
+        return mapper.toDto(sapUser);
+    }
+
+    @Transactional(rollbackFor = Exception.class, readOnly = true)
+    public SapUserDTO findByUserCode(String userCode) {
+        SapUser sapUser = repository.findByUserCode(userCode).orElse(new SapUser());
+        return mapper.toDto(sapUser);
+    }
+
     @Transactional(rollbackFor = Exception.class, readOnly = true)
     public List<SapUser> findAll() {
         return repository.findAll();
+    }
+
+    @Transactional(rollbackFor = Exception.class, readOnly = true)
+    public Boolean isExist(String userCode) {
+        return repository.findByUserCode(userCode).isPresent();
     }
 }
--- src/main/java/com/swiftcode/web/rest/SapUserResource.java
+++ src/main/java/com/swiftcode/web/rest/SapUserResource.java
@@ -4,11 +4,16 @@ import com.swiftcode.domain.SapUser;
 import com.swiftcode.service.SapUserService;
 import com.swiftcode.service.dto.SapUserDTO;
 import com.swiftcode.service.util.CommonResult;
+import com.swiftcode.web.rest.vm.ManagedSapUserVm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author chen
@@ -30,10 +35,32 @@ public class SapUserResource {
         return CommonResult.success(sapUser, "添加成功");
     }
 
+    @ApiOperation("SAP USER 更新")
+    @PostMapping("/updateUser")
+    public CommonResult<SapUserDTO> updateSapUser(@RequestBody ManagedSapUserVm managedSapUserVm) {
+        SapUserDTO sapUser = service.updateSapUser(managedSapUserVm);
+        return CommonResult.success(sapUser, "更新成功");
+    }
+
+    @ApiOperation("SAP USER 查询")
+    @GetMapping("/user/{userCode}")
+    public CommonResult<SapUserDTO> findSapUser(@PathVariable String userCode) {
+        SapUserDTO sapUser = service.findByUserCode(userCode);
+        return CommonResult.success(sapUser, "查找成功");
+    }
+
     @ApiOperation("SAP USER 列表")
     @GetMapping("/users")
     public CommonResult<List<SapUser>> findAll() {
         List<SapUser> list = service.findAll();
         return CommonResult.success(list);
+    }
+
+    @ApiOperation("SAP USER 是否存在")
+    @GetMapping("/exist/{userCode}")
+    public ResponseEntity<Map<String, Boolean>> isExist(@PathVariable String userCode) {
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("isExist", service.isExist(userCode));
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
